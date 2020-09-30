8a4bd5d2d0ca361ec7713c0c14b89a6e217591f2
Aurobinda Mondal <die4java@gmail.com>
2020-09-29 23:05:11 -0400

    added comments

--- week1_basic_data_structures/1_brackets_in_code/check_brackets.java
+++ week1_basic_data_structures/1_brackets_in_code/check_brackets.java
@@ -3,12 +3,23 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.Stack;
 
+/**
+ * @author aurobindamondal
+ * To solve this problem, you can slightly modify the IsBalanced algorithm from the lectures 
+ * to account not only for the brackets, but also for other characters in the code,
+ *  and return not just whether the code uses brackets correctly, but also what is the 
+ *  first position where the code becomes broken.
+ */
 class Bracket {
     Bracket(char type, int position) {
         this.type = type;
         this.position = position;
     }
 
+    /**
+     * @param c
+     * @return
+     */
     boolean Match(char c) {
         if (this.type == '[' && c == ']')
             return true;
@@ -23,6 +34,10 @@ class Bracket {
     int position;
 }
 
+/**
+ * @author aurobindamondal
+ *
+ */
 class check_brackets {
     public static void main(String[] args) throws IOException {
         InputStreamReader input_stream = new InputStreamReader(System.in);
@@ -33,24 +48,25 @@ class check_brackets {
         for (int position = 0; position < text.length(); ++position) {
             char next = text.charAt(position);
 
+            // Processing opening bracket, pushing to stack
             if (next == '(' || next == '[' || next == '{') {
                 opening_brackets_stack.push(new Bracket(next, position));
             }
-
+            // Processing closing bracket
             if (next == ')' || next == ']' || next == '}') {
-                Bracket prevElement = opening_brackets_stack.pop();
+                Bracket prevElement = opening_brackets_stack.pop(); //get previous element
                 char prev = prevElement.type;
-                if((next == ')' && prev != '(') || (next == ']' && prev != '[') || (next == '}' && prev != '{')){
-                    System.out.println(position+1);
+                if((next == ')' && prev != '(') || (next == ']' && prev != '[') || (next == '}' && prev != '{')){ //if valid
+                    System.out.println(position+1); //print position
                     break;
                 }   
             }
         }
 
-        // Printing answer, write your code here
+        // Printing answer
         if(opening_brackets_stack.isEmpty())
-            System.out.println("Success");
-        else
+            System.out.println("Success"); //if stack is empty
+        else //if the stack is not empty
             System.out.println(opening_brackets_stack.pop().position+1);
     }
 }
--- week1_basic_data_structures/2_tree_height/tree_height.java
+++ week1_basic_data_structures/2_tree_height/tree_height.java
@@ -1,20 +1,44 @@
 import java.util.*;
 import java.io.*;
 
+/**
+ * @author 
+ * To solve this problem, change the height function described in the lectures 
+ * with an implementation which will work for an arbitrary tree. 
+ * Note that the tree can be very deep in this problem, 
+ * so you should be careful to avoid stack overflow problems if you’re using recursion, 
+ * and definitely test your solution on a tree with the maximum possible height.
+ */
 public class tree_height {
-	private TreeNode root;
+	private TreeNode root; //to keep track of the root
+	
+	/**
+	 * @author aurobindamondal
+	 * my own Tree implementation class
+	 */
 	class TreeNode{
 		int val;
 		List<TreeNode> children;
 		
+		/**
+		 * @param val
+		 * constructor
+		 */
 		TreeNode(int val){
 			this.val= val;
 		}
 		
+		/**
+		 * @param node
+		 * @returnList<TreeNode>
+		 */
 		List<TreeNode> getChildren(TreeNode node) {
 			return node.children;
 		}
 		
+		/**
+		 * @param child
+		 */
 		void setChildren(TreeNode child) {
 			if(this.children!=null)
 				this.children.add(child);
@@ -25,6 +49,10 @@ public class tree_height {
 				
 		}
 	}
+    /**
+     * 
+     *
+     */
     class FastScanner {
 		StringTokenizer tok = new StringTokenizer("");
 		BufferedReader in;
@@ -33,48 +61,67 @@ public class tree_height {
 			in = new BufferedReader(new InputStreamReader(System.in));
 		}
 
+		/**
+		 * @return
+		 * @throws IOException
+		 */
 		String next() throws IOException {
 			while (!tok.hasMoreElements())
 				tok = new StringTokenizer(in.readLine());
 			return tok.nextToken();
 		}
+		/**
+		 * @return
+		 * @throws IOException
+		 */
 		int nextInt() throws IOException {
 			return Integer.parseInt(next());
 		}
 	}
 
+	/**
+	 * @author 
+	 *
+	 */
 	public class TreeHeight {
 		int n;
 		int parent[];
-		TreeNode[] nodes;
+		TreeNode[] nodes; //to store all the nodes
 		
+		/**
+		 * @throws IOException
+		 */
 		void read() throws IOException {
 			FastScanner in = new FastScanner();
 			n = in.nextInt();
 			parent = new int[n];
-			nodes = new TreeNode[n];
+			nodes = new TreeNode[n]; //initialize
 			for (int i = 0; i < n; i++) {
 				parent[i] = in.nextInt();
-				if(nodes[i]==null)
+				if(nodes[i]==null) //if doesn't already exist
 					nodes[i]=new TreeNode(i);
 				
-				if(parent[i] == -1)
+				if(parent[i] == -1) //if parent is -1 that means it is root
 					root = nodes[i];
 				else {
-					if(nodes[parent[i]] != null)
+					if(nodes[parent[i]] != null) //if parent already exist just add child
 						nodes[parent[i]].setChildren(nodes[i]);
-					else {
-						TreeNode parentNode = new TreeNode(parent[i]);
-						parentNode.setChildren(nodes[i]);
-						nodes[parent[i]] = parentNode;
+					else { //if parent doesn't yet exist
+						TreeNode parentNode = new TreeNode(parent[i]); //create a new parent node
+						parentNode.setChildren(nodes[i]); //set child
+						nodes[parent[i]] = parentNode; //add to the node
 					}
 				}
 					
 			}
-			//printTree(root);
+			//printTree(root);//if you want to traverse the tree
 			
 		}
 
+		/**
+		 * @param node
+		 * traverse the tree
+		 */
 		private void printTree(TreeNode node) {
 			if(node==null)
 				return;
@@ -89,17 +136,22 @@ public class tree_height {
 			}
 			
 		}
+		//to get max height
 		int maxHeight = 0;
 		
+		/**
+		 * @return max height
+		 */
 		int computeHeight() {
 			int height = 0;
+			//BFS implementation
 			Queue<TreeNode> q = new LinkedList<>();
 			if(root==null) {
 				return 0;
 			}else {
 				q.offer(root);
 				while(!q.isEmpty()) {
-					++height;
+					++height; //increase height at each level
 					int size = q.size();
 					for(int i=0; i<size; i++) {
 						TreeNode node = q.poll();
@@ -109,7 +161,7 @@ public class tree_height {
 							}
 						}	
 					}
-					maxHeight = Math.max(height, maxHeight);
+					maxHeight = Math.max(height, maxHeight); //calculate max height
 				}
 			}
 			return maxHeight;
@@ -117,6 +169,10 @@ public class tree_height {
 
 	}
 
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
 	static public void main(String[] args) throws IOException {
             new Thread(null, new Runnable() {
                     public void run() {
@@ -127,6 +183,9 @@ public class tree_height {
                     }
                 }, "1", 1 << 26).start();
 	}
+	/**
+	 * @throws IOException
+	 */
 	public void run() throws IOException {
 		TreeHeight tree = new TreeHeight();
 		tree.read();
